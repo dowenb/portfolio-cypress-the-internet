d1cb01cb774c5e4a32df5632ea0dc8736ab619ba
Ben Dowen <33220797+dowenb@users.noreply.github.com>
2023-05-16 20:16:02 +0100

    improve add remove elements test

--- cypress/e2e/add_remove_elements.cy.js
+++ cypress/e2e/add_remove_elements.cy.js
@@ -7,14 +7,26 @@ describe('Add/Remove Elements', () => {
     cy.get('#elements').children().should('have.length', 0)
   })
 
-  it('Elements can be added and removed', () => {
+  it('Elements can be added', () => {
     cy.visit('/add_remove_elements/')
 
     cy.get('#content > div > button').click()
 
     cy.get('#elements').children().should('have.length', 1)
+  })
 
-    cy.get('#elements > button').click()
+  it('All added elements can be removed', () => {
+    cy.visit('/add_remove_elements/')
+
+    // TODO(later): Better way to add n elements
+    cy.get('#content > div > button').click()
+    cy.get('#content > div > button').click()
+    cy.get('#content > div > button').click()
+
+    // TODO(later): Make this easier to read, so it's obvious it clicks each delete button
+    cy.get('#elements > button').each(($el) => {
+      cy.wrap($el).click()
+    })
 
     cy.get('#elements').children().should('have.length', 0)
   })
